38b49bd8577cf89e54767640c3977cd288ac3f4c
rodrigodevneoris <p-rpozo@neoris.com>
2024-10-16 21:14:26 +0200

    API completa funcionando.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,7 +2,6 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { json } from 'body-parser';
-import authRoutes from './routes/auth.routes';
 import { getDb } from './db'; // Asegúrate de importar la función getDb
 
 const app = express();
@@ -22,8 +21,6 @@ app.use(bodyParser.json());
         console.error("Error al establecer la conexión a la base de datos:", error.message); // Mensaje de error
     }
 })();
-// Rutas de autenticación
-app.use('/api/auth', authRoutes);
 
 // Obtener todos los productos
 app.get('/api/products', async (req: Request, res: Response, next: NextFunction) => {
